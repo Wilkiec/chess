b7e9d5d09dd94272fb17885b7eb4267842996676
Corben Wilkie <corben.wilkie55@gmail.com>
2025-10-31 04:09:03 -0600

    gameDataDAO is fully functional with sql. Now i just gotta do userdata and the other one. Should be easier. Figured out why my db wasn't connecting either: it was cuz intellij is on wsl and I installed sql on windows

--- server/src/main/java/dataaccess/GameDataDAO.java
+++ server/src/main/java/dataaccess/GameDataDAO.java
@@ -28,7 +28,11 @@ public class GameDataDAO extends DatabaseManager {
         try (var conn = DatabaseManager.getConnection()) {
             var preparedStatement = conn.prepareStatement(sqlScript);
             var rs = preparedStatement.executeQuery();
-            return rs.getInt(1) == 1;
+
+            if (rs.next()) {
+                return rs.getInt(1) == 0;
+            }
+            return false;
         } catch (SQLException | DataAccessException e) {
             throw new RuntimeException(e);
         }
@@ -56,11 +60,12 @@ public class GameDataDAO extends DatabaseManager {
     }
 
     public static int createGame(String gameName) {
-        String gameNameAlrExist = "SELECT 1 FROM gameData WHERE gameName = " + gameName;
+        String gameNameAlrExist = "SELECT 1 FROM gameData WHERE gameName = ?";
         String insertGameName = "INSERT INTO gameData (gameID, gameName, chessGame) VALUES (?, ?, ?)";
 
         try (var conn = DatabaseManager.getConnection()) {
             var preparedStatement = conn.prepareStatement(gameNameAlrExist);
+            preparedStatement.setString(1, gameName);
             var rs = preparedStatement.executeQuery();
             if (rs.next()) {
                 throw new BadRequestException("bad request");
@@ -71,8 +76,8 @@ public class GameDataDAO extends DatabaseManager {
             // turning chessGame into JSON.
             var serializer = new Gson();
             var chessGameJSON = serializer.toJson(new ChessGame());
-            ps.setString(1, gameName);
-            ps.setInt(2, uniqueId);
+            ps.setInt(1, uniqueId);
+            ps.setString(2, gameName);
             ps.setString(3, chessGameJSON);
             ps.executeUpdate();
             return uniqueId;
@@ -82,24 +87,34 @@ public class GameDataDAO extends DatabaseManager {
     }
 
     public static void joinGame(int gameID, ChessGame.TeamColor teamColor, String username) {
-        String gameIdExist = "SELECT 1 FROM gameData WHERE gameID = " + gameID;
-
+        String userAlrExists;
         String insertGameName;
         insertGameName = "UPDATE gameData SET usernameWhite = ? WHERE gameID = ?";
+        userAlrExists = "SELECT usernameWhite FROM gameData WHERE gameID = ? ";
         if (teamColor == ChessGame.TeamColor.BLACK) {
             insertGameName = "UPDATE gameData SET usernameBlack = ? WHERE gameID = ?";
+            userAlrExists = "SELECT usernameBlack FROM gameData WHERE gameID = ? ";
         }
 
         try (var conn = DatabaseManager.getConnection()) {
-            // checking to make sure the game has been created and not trying to join a non existant game
-            var preparedStatement = conn.prepareStatement(gameIdExist);
+            // checking to make sure the game has been created and not trying to join a non-existent game
+            var preparedStatement = conn.prepareStatement(userAlrExists);
+            preparedStatement.setInt(1, gameID);
             var rs = preparedStatement.executeQuery();
-            if (rs.next()) {
+            if (!rs.next()) {
                 throw new BadRequestException("bad request");
+            }
+
+            // finding out if the color user wants to join is already taken
+            String existingUsername = rs.getString(1);
+            if (existingUsername != null) {
+                throw new AlreadyTakenException("already taken");
             }
 
             // inserting username into the specific team color
             preparedStatement = conn.prepareStatement(insertGameName);
+            preparedStatement.setString(1, username);
+            preparedStatement.setInt(2, gameID);
             preparedStatement.executeUpdate();
         } catch (SQLException | DataAccessException e) {
             throw new RuntimeException(e);
