662e60d76c6a27ac0c1a6921bd0fffa694704cff
Corben Wilkie <corben.wilkie55@gmail.com>
2025-12-04 12:54:43 -0700

    setting up inGameClient Commands

--- client/src/main/java/client/InGameClient.java
+++ client/src/main/java/client/InGameClient.java
@@ -14,23 +14,55 @@ public class InGameClient implements ReplClient {
             var tokens = line.toLowerCase().split(" ");
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
-            if (cmd.equals("quit")) {
-                return "quit";
-            } else if (cmd.equals("back")) {
-                return back(params);
-            } else {
-                return "Invalid Argument";
-            }
+            return switch (cmd) {
+                case "leave" -> leave(params);
+                case "redraw" -> redraw(params);
+                case "highlight" -> highlightLegalMoves(params);
+                case "resign" -> resign(params);
+                case "move" -> makeMove(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
         } catch (ResponseException ex) {
             return ex.getMessage();
         }
     }
 
-    public String back(String[] params) throws ResponseException {
-        if (params.length != 0) {
-            return "Back Command Takes No Inputs";
-        }
-        repl.setLoggedIn(repl.authToken);
-        return "successfully went back";
+    private String makeMove(String[] params) {
+        return "";
     }
+
+    private String resign(String[] params) {
+        return "";
+    }
+
+    private String highlightLegalMoves(String[] params) {
+        return "";
+    }
+
+    private String redraw(String[] params) {
+        return "";
+    }
+
+    public String leave(String[] params) throws ResponseException {
+        if (params.length != 0) {
+            return "leave Command Takes No Inputs";
+        }
+
+        repl.setLoggedIn(repl.authToken);
+        return "successfully left game";
+    }
+
+    public String help() throws ResponseException {
+        return """
+                highlight <Piece Position> - highlight all legal moves for a given piece
+                move <Start Position> <End Position> - make a move!
+                resign - forfeit the chess match
+                leave - go back to menu (doesn't forfeit the game)
+                quit - Exit application
+                help - Learn the commands
+                """;
+    }
+
+
 }
